34684800c3dcde361f5224ac2c569dbccfec8413
narcolin <narcolin@calpoly.edu>
2023-05-31 17:02:13 -0700

    shield player animation

--- Assets/Scripts/ShieldController.cs
+++ Assets/Scripts/ShieldController.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShieldController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ShieldController : MonoBehaviour {
+    public Animator myAnim;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start(){
+        myAnim = GetComponent<Animator>();
+    }
+    IEnumerator OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Player"){
+            Destroy(gameObject);
+            myAnim.Play("PlayerShieldAnimation");
+            yield return new WaitForSeconds(10.0f);
+        }
     }
 }
